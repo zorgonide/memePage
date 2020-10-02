dc6adf7928d58bbbfc6f34fd602d0cc0fabcb19c
zorgonide <ammar1998@gmail.com>
2020-10-02 21:05:33 +0530

    Published

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,20 +5,20 @@ export class Main extends Component {
         super(props);
         this.state = {
             img: 'sad',
-            topText: ["Mech+bois", "Civil+bois", "IP+bois"],
-            bottomText: ["Placement+nhi+bhaukal", "#Chaud", "Jaldi+se+dood+pee+leta+hun", "Professional+Virgins", "Civil+chutiya"],
+            topText: ["Mech+bois", "Civil+bois", "IP+bois", "ICE+bois"],
+            bottomText: ["Placement+nhi+bhaukal", "Chaud", "Jaldi+se+dood+pee+leta+hun", "Professional+Virgins", "Civil+chutiya", "TT+core+committee+for+lyf", "Company+nhi+ladki+milti+humme", "Mummy+ka+laadla", "Na+dare+na+bhide+jab+mann+kare+OT+me+tapak+jaaye" , "Tiktok+is+passion" , "4LPA+job+andhapaisa", "cringeee", "Maths+2+makeup+time"],
             options: data,
         };
     }
     render() {
-        let str = `http://apimeme.com/meme?meme=${this.state.options[Math.floor(Math.random()*1700)]}&top=${this.state.topText[Math.floor(Math.random()*this.state.topText.length)]}&bottom=${this.state.bottomText[Math.floor(Math.random()*this.state.bottomText.length)]}&test=1`
+        let str = `http://apimeme.com/meme?meme=${this.state.options[Math.floor(Math.random()*this.state.options.length)]}&top=${this.state.topText[Math.floor(Math.random()*this.state.topText.length)]}&bottom=${this.state.bottomText[Math.floor(Math.random()*this.state.bottomText.length)]}&test=1`
         
         return (
             <div className="container">
                 <div className="col">
                     <div className="d-flex justify-content-center align-items-center login-container">
-                        <div className="card border p-10">
-                            <h1 className="text-danger">Hello, I'm Bhaukal</h1>
+                        <div className="card border text-center">
+                            <h1 className="text-danger ">Hello, I'm Bhaukal</h1>
                             <a href={str} className="btn btn-danger">For proof click here</a>
                         </div>
                     </div>
